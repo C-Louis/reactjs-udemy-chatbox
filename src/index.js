5821230d9700991485236bc253ed5adea33d0ea9
Cynthia <cynthialouis.dev@gmail.com>
2017-10-10 11:52:37 +0200

    Setting up React.Component and Render to display a form

--- src/index.js
+++ src/index.js
@@ -1,1 +1,31 @@
-// Code React.js
+//import react lib
+import React from 'react';
+import { render } from 'react-dom';
+//CSS
+import './index.css';
+
+//Create new component Connexion
+class Connexion extends React.Component {
+    render() {
+        return (
+            // return jsx
+            <div className="connexionBox">
+                <form className="connexion">
+                    <input
+                        type="text"
+                        placeholder="Pseudo"
+                        required
+                    />
+                    <button type="submit">GO</button>
+                </form>
+            </div>
+        )
+    }
+}
+
+//To display
+render(
+    //the component Connexion
+    <Connexion/>,
+    document.getElementById('root')
+);
