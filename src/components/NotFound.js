b3d7fcbe602a85d57586d174b0d6f4696d9dcd5b
Cynthia <cynthialouis.dev@gmail.com>
2017-10-10 12:08:52 +0200

    Starting with React Router

---
+++ src/components/NotFound.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+//Steless component (only to display)
+const NotFound = () => {
+    // return jsx
+    return (
+        <h2 className="notFound">NOT FOUND !</h2>
+    )
+};
+
+export default NotFound;
--- src/index.js
+++ src/index.js
@@ -4,12 +4,30 @@ import { render } from 'react-dom';
 //Components
 import Connexion from './components/Connexion'
 import App from './components/App'
+import NotFound from './components/NotFound'
+//Rooter
+import { BrowserRouter, Match, Miss } from 'react-router';
 //CSS
 import './index.css';
+
+//Stateless component router that only displays something (no logic behind)
+const Root = () => {
+    return (
+        //For different route options
+        <BrowserRouter>
+            {/*In BrowerRouter, components need to be wrap into a div*/}
+            <div>
+                <Match exactly pattern="/" component={Connexion}/>
+                <Match pattern="/pseudo/:pseudo" component={App}/>
+                <Miss component={NotFound}/>
+            </div>
+        </BrowserRouter>
+    )
+};
 
 //To display
 render(
     //the component Connexion
-    <App/>,
+    <Root/>,
     document.getElementById('root')
 );
