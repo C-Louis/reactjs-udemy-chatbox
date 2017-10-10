a8eeeeba3ba9013a4c79ce2d999920ce59537bbb
Cynthia <cynthialouis.dev@gmail.com>
2017-10-10 12:17:54 +0200

    Starting using EVENTS

--- src/components/Connexion.js
+++ src/components/Connexion.js
@@ -3,15 +3,24 @@ import React from 'react';
 
 //Create new component
 class Connexion extends React.Component {
+
+    goToChat = event => {
+        event.preventDefault();
+        //Get the pseudo thanks to the input REF (faster for React)
+        const pseudo = this.pseudoInput.value;
+        //Change url
+    };
+
     render() {
         // return jsx
         return (
-            <div className="connexionBox">
+            <div className="connexionBox" onSubmit={e => this.goToChat(e)}>
                 <form className="connexion">
                     <input
                         type="text"
                         placeholder="Pseudo"
                         required
+                        ref={input => { this.pseudoInput = input}}
                     />
                     <button type="submit">GO</button>
                 </form>
